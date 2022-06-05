d1dc949d685eb80f8fb7f7d3a393b49791f6219f
Dawid Lachowicz <domdaw7@gmail.com>
2022-06-05 11:29:03 +0100

    Add user query by id

--- resolvers/user_query.go
+++ resolvers/user_query.go
@@ -1,6 +1,9 @@
 package resolvers
 
 import (
+	"strconv"
+
+	"github.com/graph-gophers/graphql-go"
 	"gorm.io/gorm"
 
 	"github.com/dawidl022/go-fake-api/models"
@@ -16,11 +19,25 @@ func NewUserQuery(db *gorm.DB) *UserQuery {
 
 func (u *UserQuery) Users() []*User {
 	var users []*models.User
-	u.db.Find(&users)
+	u.db.Preload("Address").Preload("Company").Find(&users)
 
 	var resolvers []*User
 	for _, user := range users {
 		resolvers = append(resolvers, &User{u: user})
 	}
 	return resolvers
+}
+
+type userByIdArgs struct {
+	ID graphql.ID
+}
+
+func (u *UserQuery) UserById(args userByIdArgs) (*User, error) {
+	var user models.User
+	id, err := strconv.Atoi(string(args.ID))
+	if err != nil {
+		return nil, err
+	}
+	err = u.db.Joins("Address").Joins("Company").First(&user, id).Error
+	return &User{u: &user}, err
 }
